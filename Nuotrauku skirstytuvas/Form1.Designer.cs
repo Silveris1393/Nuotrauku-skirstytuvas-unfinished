f093a63aa14f3869e11392643ba19e8bbb4be549
Dell <Dell@DESKTOP-I12KEI5>
2022-03-18 18:51:43 +0200

    Add project files.

---
+++ Nuotrauku skirstytuvas/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Nuotrauku_skirstytuvas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.pasirinktiIsKur = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.isKurLangas = new System.Windows.Forms.TextBox();
+            this.iKur = new System.Windows.Forms.TextBox();
+            this.pasirinktiIKur = new System.Windows.Forms.Button();
+            this.failuKiekisLangas = new System.Windows.Forms.TextBox();
+            this.kopijuoti = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "iskur";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "į kur";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // pasirinktiIsKur
+            // 
+            this.pasirinktiIsKur.Location = new System.Drawing.Point(568, 55);
+            this.pasirinktiIsKur.Name = "pasirinktiIsKur";
+            this.pasirinktiIsKur.Size = new System.Drawing.Size(75, 23);
+            this.pasirinktiIsKur.TabIndex = 1;
+            this.pasirinktiIsKur.Text = "Pasirinkti";
+            this.pasirinktiIsKur.UseVisualStyleBackColor = true;
+            this.pasirinktiIsKur.Click += new System.EventHandler(this.pasirinktiIsKur_Click);
+            // 
+            // isKurLangas
+            // 
+            this.isKurLangas.Location = new System.Drawing.Point(60, 55);
+            this.isKurLangas.Name = "isKurLangas";
+            this.isKurLangas.Size = new System.Drawing.Size(409, 20);
+            this.isKurLangas.TabIndex = 2;
+            // 
+            // iKur
+            // 
+            this.iKur.Location = new System.Drawing.Point(60, 221);
+            this.iKur.Name = "iKur";
+            this.iKur.Size = new System.Drawing.Size(409, 20);
+            this.iKur.TabIndex = 2;
+            // 
+            // pasirinktiIKur
+            // 
+            this.pasirinktiIKur.Location = new System.Drawing.Point(568, 221);
+            this.pasirinktiIKur.Name = "pasirinktiIKur";
+            this.pasirinktiIKur.Size = new System.Drawing.Size(75, 23);
+            this.pasirinktiIKur.TabIndex = 1;
+            this.pasirinktiIKur.Text = "Pasirinkti";
+            this.pasirinktiIKur.UseVisualStyleBackColor = true;
+            this.pasirinktiIKur.Click += new System.EventHandler(this.pasirinktiIKur_Click);
+            // 
+            // failuKiekisLangas
+            // 
+            this.failuKiekisLangas.Location = new System.Drawing.Point(60, 92);
+            this.failuKiekisLangas.Multiline = true;
+            this.failuKiekisLangas.Name = "failuKiekisLangas";
+            this.failuKiekisLangas.Size = new System.Drawing.Size(409, 70);
+            this.failuKiekisLangas.TabIndex = 3;
+            // 
+            // kopijuoti
+            // 
+            this.kopijuoti.Location = new System.Drawing.Point(325, 340);
+            this.kopijuoti.Name = "kopijuoti";
+            this.kopijuoti.Size = new System.Drawing.Size(75, 23);
+            this.kopijuoti.TabIndex = 4;
+            this.kopijuoti.Text = "Kopijuoti";
+            this.kopijuoti.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.kopijuoti.UseVisualStyleBackColor = true;
+            this.kopijuoti.Click += new System.EventHandler(this.kopijuoti_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(730, 406);
+            this.Controls.Add(this.kopijuoti);
+            this.Controls.Add(this.failuKiekisLangas);
+            this.Controls.Add(this.iKur);
+            this.Controls.Add(this.isKurLangas);
+            this.Controls.Add(this.pasirinktiIKur);
+            this.Controls.Add(this.pasirinktiIsKur);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Button pasirinktiIsKur;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox isKurLangas;
+        private System.Windows.Forms.TextBox iKur;
+        private System.Windows.Forms.Button pasirinktiIKur;
+        private System.Windows.Forms.TextBox failuKiekisLangas;
+        private System.Windows.Forms.Button kopijuoti;
+    }
+}
+
---
+++ Nuotrauku skirstytuvas/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Nuotrauku_skirstytuvas
+{
+    public partial class Form1 : Form
+    {
+       FolderBrowserDialog folderBrowserDialog2 = new FolderBrowserDialog();
+        static string fName;
+        string pirmasPasirinkimas;
+        string antrasPasirinkimas;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pasirinktiIsKur_Click(object sender, EventArgs e)
+        {
+           
+            folderBrowserDialog1.ShowDialog();
+            isKurLangas.Text = folderBrowserDialog1.SelectedPath;
+            int kiekYra = Directory.GetFiles(folderBrowserDialog1.SelectedPath).Count();
+            failuKiekisLangas.Text = $"isviso lange yra {kiekYra} failu";
+
+            // folderBrowserDialog2.RootFolder = Environment.SpecialFolder;
+            pirmasPasirinkimas = folderBrowserDialog1.SelectedPath;
+
+        }
+
+        private void pasirinktiIKur_Click(object sender, EventArgs e)
+        {
+
+           
+
+            folderBrowserDialog2.ShowDialog();
+            iKur.Text = folderBrowserDialog2.SelectedPath;
+           string[] fileEntries = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg");
+
+            foreach (string fileName in fileEntries)
+            {    fName = fileName.Substring(pirmasPasirinkimas.Length +1 );
+                ProcessFile(fName);
+               
+            }
+
+            antrasPasirinkimas = folderBrowserDialog2.SelectedPath;
+
+            // System.IO.File.Copy(files[0], kurPath, true);
+            /*
+            foreach (string f in files)
+            {
+                if (File.Exists(f))
+                {
+                   
+                    System.IO.File.Copy(f, kurPath, true);
+                }
+            }
+            */
+        }
+
+        private void kopijuoti_Click(object sender, EventArgs e)
+        {
+            string isKurPath = folderBrowserDialog1.SelectedPath;
+            string kurPath = folderBrowserDialog2.SelectedPath;
+            
+            string[] files = Directory.GetFiles(isKurPath);
+         
+         
+            foreach (string f in files)
+            {
+                string naujasFolderis = Path.Combine(kurPath, "testas");
+                Directory.CreateDirectory(naujasFolderis);
+                fName = f.Substring(pirmasPasirinkimas.Length + 1);
+                
+                File.Copy(Path.Combine(isKurPath, fName),Path.Combine(kurPath, fName), true);
+                ProcessFile(fName);
+                
+            }
+
+        }
+        public void ProcessFile(string path)
+        {
+            failuKiekisLangas.Text += $" failo pavadinimas {path} ";
+        }
+
+    }
+}
